bc629e5d0934ef7e86f7961e9d710abf1452472a
Felipe Villa <villajaramillofelipe4@gmail.com>
2025-09-11 13:34:12 -0500

    feat(entities): Add User & Role entities

---
+++ src/entities/Role.ts
@@ -0,0 +1,14 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from './User';
+
+@Entity('roles')
+export class Role {
+  @PrimaryGeneratedColumn()
+  role_id: number;
+
+  @Column({ type: 'varchar', length: 20, nullable: false })
+  role_name: string;
+
+  @OneToMany(() => User, (user) => user.role)
+  users: User[];
+}
---
+++ src/entities/User.ts
@@ -0,0 +1,35 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Exclude } from 'class-transformer';
+import { Role } from './Role';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  user_id: number;
+
+  @Column({ type: 'varchar', length: 25, nullable: false })
+  name: string;
+
+  @Column({ type: 'varchar', length: 254, nullable: false, unique: true })
+  email: string;
+
+  @Column({ type: 'varchar', length: 60, nullable: false })
+  @Exclude()
+  password: string;
+
+  @Column()
+  state_id: number;
+
+  @Column()
+  role_id: number;
+
+  @ManyToOne(() => Role, (role) => role.users)
+  @JoinColumn({ name: 'role_id' })
+  role: Role;
+}
