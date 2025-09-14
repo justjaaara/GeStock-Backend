4d6a5e136627c3a62609c3fe5a1988e67b43e682
Felipe Villa <villajaramillofelipe4@gmail.com>
2025-09-14 01:59:54 -0500

    feat(app): add DatabaseModule and global ConfigModule to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,18 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { RoleProviders } from './providers/role-providers/role-providers';
 import { UserProviders } from './providers/user-providers/user-providers';
+import { DatabaseModule } from './config/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    DatabaseModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // hace que las variables env estén disponibles en toda la app
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService, RoleProviders, UserProviders],
+  providers: [AppService, ...RoleProviders, ...UserProviders],
 })
 export class AppModule {}
