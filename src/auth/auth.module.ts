12f614acc62613cb0160c1f0f972c5044bf4da36
Felipe Villa <villajaramillofelipe4@gmail.com>
2025-09-14 01:35:07 -0500

    feat(auth): integrate JwtModule and configure CORS in main application

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { DatabaseModule } from 'src/config/database.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt-auth.strategie';
+import { UserProviders } from 'src/providers/user-providers/user-providers';
+import { roleProviders } from 'src/providers/role-providers/role-providers';
 
 @Module({
+  imports: [
+    DatabaseModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'LLAVE-SECRETA',
+      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN || '1d' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy, ...UserProviders, ...roleProviders],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 import 'reflect-metadata';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //CORS
+  app.enableCors({
+    oriign: 'http://localhost:4200',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  //Habilitar validación global
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
