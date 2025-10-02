11100e314fd64930852bc1150fd136d01960522b
Juan Pablo <jpcardonab0520@gmail.com>
2025-10-02 11:09:14 -0500

    featL add configuration and user modules to main application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './config/database.module';
 import { roleProviders } from './providers/role-providers/role-providers';
 import { UserProviders } from './providers/user-providers/user-providers';
-import { DatabaseModule } from './config/database.module';
-import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true, // hace que las variables env estén disponibles en toda la app
       envFilePath: '.env',
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService, ...roleProviders, ...UserProviders],
