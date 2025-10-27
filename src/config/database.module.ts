d1142ede3f7a9c21ffc9969f9e1498cfbbd8043b
Felipe Villa <villajaramillofelipe4@gmail.com>
2025-10-27 18:35:22 -0500

    fix(seeding): Fixed seeding errors, missing imports

--- src/config/database.module.ts
+++ src/config/database.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Role } from '../entities/Role.entity';
+import { UserState } from '../entities/User-state.entity';
+import { User } from '../entities/User.entity';
+import { MeasurementType } from '../entities/Measurement-type.entity';
+import { ProductCategory } from '../entities/Product-category.entity';
+import { ProductState } from '../entities/Product-state.entity';
 import { SeederService } from './seeder.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Role])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Role,
+      UserState,
+      User,
+      MeasurementType,
+      ProductCategory,
+      ProductState,
+    ]),
+  ],
   providers: [SeederService],
   exports: [SeederService],
 })
